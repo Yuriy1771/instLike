9a70b4463dfecd249c69319ea0553a463c5a5536
Yuriy <dementev.yuriy@list.ru>
2022-03-07 13:28:01 +0300

    test

--- scriptLike.js
+++ scriptLike.js
@@ -1,7 +1,7 @@
 let imgLike = document.querySelector('#instagramHert');
 let photoInst = document.querySelector('#imgInWindow');
-
+let name;
 
 photoInst.ondblclick = () => {
     imgLike.style.display = 'block';
-}
+} 
