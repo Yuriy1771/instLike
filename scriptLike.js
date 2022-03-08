feaa03b80aff28d1aeec02767f57fcf582063c1b
Yuriy <dementev.yuriy@list.ru>
2022-03-08 13:43:24 +0300

    added logo,header,border

--- scriptLike.js
+++ scriptLike.js
@@ -2,6 +2,23 @@ let imgLike = document.querySelector('#instagramHert');
 let photoInst = document.querySelector('#imgInWindow');
 let name;
 
+// photoInst.ondblclick = () => {
+//     imgLike.style.display = 'block';
+// } 
+
+// if(imgLike == true) {
+//     photoInst.ondblclick = () => {
+//             imgLike.style.display = 'block';
+//         } 
+// } else {
+//     imgLike.style.display = 'none';
+// }
+
 photoInst.ondblclick = () => {
     imgLike.style.display = 'block';
-} 
+    if(photoInst.ondblclick) {
+
+    }else {
+    imgLike.style.display = 'none';
+    }
+}
