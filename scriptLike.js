0bd117b380ed8dead7093d8967af1e4b454d816d
Yuriy <dementev.yuriy@list.ru>
2022-03-10 21:49:58 +0300

    Изменил ники, поправил код js и тд

--- scriptLike.js
+++ scriptLike.js
@@ -1,18 +1,41 @@
 let imgLike = document.querySelector('#instagramHert');
-let photoInst = document.querySelector('.imgInWindow');
 let likeBtn = document.querySelector('#likeBtn');
-
+let photoInst = document.querySelector('.imgInWindow');
+let window1 = document.querySelector('.window');
 
 let isPhotoClicked = false;
 
-photoInst.ondblclick = () => {
+window1.ondblclick = () => {
+    checkLets();
+    clickedToPhoto();
+    // if(isPhotoClicked) {
+    //     imgLike.style.display = 'none';
+    //     likeBtn.style.background = 'none';
+    //     isPhotoClicked = false;
+    // }else {
+    //     imgLike.style.display = 'block';
+    //     likeBtn.style.background = 'red';
+    //     isPhotoClicked = true;
+
+    // }
+}
+
+ let wholiked;
+
+function checkLets() {
+    wholiked = document.querySelector('#wholiked');
+}
+
+function clickedToPhoto() {
     if(isPhotoClicked) {
         imgLike.style.display = 'none';
         likeBtn.style.background = 'none';
+        wholiked.innerText += '';
         isPhotoClicked = false;
     }else {
         imgLike.style.display = 'block';
         likeBtn.style.background = 'red';
+        wholiked.innerText += 'Yuriy';
         isPhotoClicked = true;
 
     }
