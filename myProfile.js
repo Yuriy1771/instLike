d93d15d04103f4e460e08fae947d586f34efb828
Yuriy <dementev.yuriy@list.ru>
2022-03-14 21:47:33 +0300

    добавил модальное окно с возможностью написать статус и вывести его на экран

---
+++ myProfile.js
@@ -0,0 +1,28 @@
+const changeStatus = document.querySelector('#status');
+let modal = document.querySelector('.myModal');
+let save = document.querySelector('#btnModalSave');
+let close = document.querySelector('#close');
+let status1 = document.querySelector('#status');
+
+changeStatus.onclick = () => {
+    modal.style.display = 'block';
+}
+
+close.onclick = () => {
+    modal.style.display = 'none';
+}
+
+window.onclick = (event) => {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+let textArea;
+
+save.onclick = () => {
+    textArea = document.querySelector('#textareaStatus').value;
+    status1.innerHTML = textArea;
+    modal.style.display = 'none';
+}
